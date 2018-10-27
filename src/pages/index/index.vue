16f441973162845645a3c873a3b6b55bc4dbfb10
luoqing <tsingroo@gmail.com>
2018-10-27 17:37:45 +0800

    add subject empty paage

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,35 +1,71 @@
 <template>
  <div>
-    <swiper indicator-dots="true" autoplay="true" interval="3000" duration="500">
+    <swiper id="banner" indicator-dots="true" autoplay="true" interval="3000" duration="500">
       <swiper-item>
-        <image mode="scaleToFill" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/34630639.jpg" style="width: 100%;height: 168px;"/>
+        <image @click="gotoPhonics()" mode="scaleToFill" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/34630639.jpg" style="width: 100%;height: 168px;"/>
       </swiper-item>
       <swiper-item>
-        <image mode="scaleToFill" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/36305421.jpg" style="width: 100%;height: 168px;"/>
+        <image @click="gotoSubjects()" mode="scaleToFill" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/36305421.jpg" style="width: 100%;height: 168px;"/>
       </swiper-item>
     </swiper>
+    <br><br><br>
     
-    <div style="display:flex;flex-direction: row;">
-      <div class="main-item">
+    <div id="main_module" style="display:flex;flex-direction: row;">
+      <div class="main-item" @click="gotoPhonics()">
         <image mode="aspectFit" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/85472759.jpg" style="width:200rpx;height:200rpx;"/>
       </div>
-      <div class="main-item">
+      <div class="main-item" @click="gotoSubjects()">
         <image mode="aspectFit" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/50769816.jpg" style="width:200rpx;height:200rpx;"/>
       </div>
       <div class="main-item">
         <image mode="aspectFit" src="http://ph1cnfby2.bkt.clouddn.com/18-10-27/80547463.jpg" style="width:200rpx;height:200rpx;"/>
       </div>
     </div>
+    <br><br>
+
+    <div>
+      <ul class="newslist">
+        <li class="news-item">
+          * 2019.01月.第四版，未排期，广大用户可向Shirley妈妈或者Shirley爸爸提建议
+        </li>
+        <li class="news-item">
+          * 2018.12月.预计发布第三版，加入手势写字，机器智能识别功能
+        </li>
+        <li class="news-item">
+          * 2018.11月.预计发布第二版，加入新的主题课模块
+        </li>
+      </ul>
+    </div>
  </div>
 </template>
 <script>
 export default {
+  methods: {
+    gotoPhonics () {
+      wx.navigateTo({
+        url: '/pages/lessons/main'
+      })
+    },
+    gotoSubjects () {
+      wx.navigateTo({
+        url: '/pages/subjects/main'
+      })
+    }
+  }
 }
 </script>
 <style scoped>
 .main-item {
   width: 200rpx;
   height: 200rpx;
-  margin: 10rpx 25rpx 10rpx 25rpx;
+  margin: 30rpx 25rpx 30rpx 25rpx;
+}
+.newslist {
+  margin: 30rpx 0 0 30rpx;
+}
+.news-item {
+  margin-bottom: 10rpx;
+  font-size:32rpx;
+  color: #222;
 }
 </style>
